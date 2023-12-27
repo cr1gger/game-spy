067df3586df1dad40e0b03b0449d2abe4dab91f8
Vladislav <vladislav.cheremuhin@convergent.digital>
2023-12-27 21:54:01 +0500

    Backend: CORS

--- backend/controllers/ApiController.php
+++ backend/controllers/ApiController.php
@@ -19,6 +19,24 @@ class ApiController extends Controller
     }
     public function behaviors()
     {
+        $behaviors = parent::behaviors();
+
+        // remove authentication filter
+        $auth = $behaviors['authenticator'];
+        unset($behaviors['authenticator']);
+
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::class,
+        ];
+
+        // re-add authentication filter
+        $behaviors['authenticator'] = $auth;
+        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
+        $behaviors['authenticator']['except'] = ['options'];
+
+        return $behaviors;
+
         return array_merge(parent::behaviors(), [
             'corsFilter' => [
                 'class' => Cors::class,
