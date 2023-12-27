732e3bf22bba5a3a70e641e439d3571f351ca6d8
Vladislav <vladislav.cheremuhin@convergent.digital>
2023-12-28 02:24:02 +0500

    Backend: CORS

--- backend/controllers/ApiController.php
+++ backend/controllers/ApiController.php
@@ -4,7 +4,7 @@ namespace app\controllers;
 
 use app\models\Category;
 use app\models\Words;
-use Yii;
+use yii\filters\Cors;
 use yii\rest\Controller;
 
 class ApiController extends Controller
@@ -19,52 +19,17 @@ class ApiController extends Controller
     }
     public function behaviors()
     {
-        $behaviors = parent::behaviors();
+        return array_merge(parent::behaviors(), [
+            'corsFilter' => [
+                'class' => Cors::class,
+                'cors' => [
+                    'Origin' => ['*'],
+                    'Access-Control-Allow-Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['POST', 'GET'],
+                ],
 
-        // remove authentication filter
-        $auth = $behaviors['authenticator'];
-        unset($behaviors['authenticator']);
-
-        // add CORS filter
-        $behaviors['corsFilter'] = [
-            'class' => \yii\filters\Cors::class,
-            'cors' => [
-                'Origin' => [Yii::$app->request->getOrigin()],
-                'Access-Control-Allow-Credentials' => true,
-                // Allow  methods
-                'Access-Control-Request-Method' => ['POST', 'PUT', 'OPTIONS', 'GET', 'DELETE'],
-                // Allow only headers 'X-Wsse'
-                'Access-Control-Request-Headers' => ['*'],
-                'Access-Control-Allow-Headers' => ['Content-Type'],
-                // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
-//                'Access-Control-Allow-Credentials' => true,
-                // Allow OPTIONS caching
-
-                'Access-Control-Max-Age' => 3600,
-                // Allow the X-Pagination-Current-Page header to be exposed to the browser.
-                'Access-Control-Expose-Headers' => ['*']
             ],
-        ];
-
-        // re-add authentication filter
-        $behaviors['authenticator'] = $auth;
-        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
-        $behaviors['authenticator']['except'] = ['options'];
-
-        return $behaviors;
-
-//        return array_merge(parent::behaviors(), [
-//            'corsFilter' => [
-//                'class' => Cors::class,
-//                'cors' => [
-//                    'Origin' => ['*'],
-//                    'Access-Control-Allow-Origin' => ['*'],
-//                    'Access-Control-Request-Method' => ['POST', 'GET'],
-//                    'Access-Control-Request-Headers' => ['*'],
-//                ],
-//
-//            ],
-//        ]);
+        ]);
     }
     public function actionIndex()
     {
